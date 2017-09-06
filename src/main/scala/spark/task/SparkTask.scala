cdf4f4bf3a7e906116cbbaf9ab95ba39c5e0f615
Sergey Ermolaev <sermolaev.1986@gmail.com>
2017-09-06 11:12:25 +0300

    done with 4th step

--- src/main/scala/spark/task/SparkTask.scala
+++ src/main/scala/spark/task/SparkTask.scala
@@ -1,6 +1,6 @@
 package spark.task
 
-import net.liftweb.json.DefaultFormats
+import net.liftweb.json.{DefaultFormats, Extraction, JsonAST, Printer}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types.StringType
 import org.apache.spark.sql.{DataFrame, SparkSession}
@@ -8,6 +8,8 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 object SparkTask {
 
   case class ColumnConfiguration(existing_col_name: String, new_col_name: String, new_data_type: String)
+
+  case class ColumnProfileInfo(column: String, unique_values: Int, values: Map[Any, Any])
 
   def main(args: Array[String]): Unit = {
     require(args.length == 3, "Provide parameters in this order: dataPath, delimiter, columnConfigs")
@@ -54,6 +56,12 @@ object SparkTask {
     df.printSchema()
     df.show(10)
 
+    println("\n-------Step 4 profile columns------\n")
+    val profileResult = profileColumns(df)
+    implicit val formats = DefaultFormats + MapSerializer
+    val profileResultAsJson = Printer.pretty(JsonAST.render(Extraction.decompose(profileResult)))
+    println(profileResultAsJson)
+
     sparkSession.stop()
   }
 
@@ -72,6 +80,9 @@ object SparkTask {
         .withColumn(columnConfig.new_col_name, col(columnConfig.new_col_name).cast(columnConfig.new_data_type))
     }
 
+    val columnsToRemove = df.schema.fieldNames.diff(columnConfigurations.map(_.existing_col_name))
+    varDF = varDF.drop(columnsToRemove: _*)
+
     varDF
   }
 
@@ -86,5 +97,38 @@ object SparkTask {
     }
 
     varDF
+  }
+
+  def profileColumns(df: DataFrame): Array[ColumnProfileInfo] = {
+    df.rdd
+      .flatMap(row =>
+        row.schema.fieldNames.zipWithIndex.map { case (fieldName, index) => ((fieldName, row(index)), 1L) }
+      )
+      .reduceByKey(_ + _)
+      .filter { case ((fieldName, fieldValue), count) => fieldValue != null }
+      .map { case ((fieldName, fieldValue), count) => (fieldName, (fieldValue, count))}
+      .groupByKey()
+      .collect()
+      .map { case (fieldName, fieldValues) => ColumnProfileInfo(fieldName, fieldValues.size, fieldValues.toMap) }
+  }
+
+  import net.liftweb.json._
+  object MapSerializer extends Serializer[Map[Any, Any]] {
+    def serialize(implicit format: Formats): PartialFunction[Any, JValue] = {
+      case m: Map[_, _] => JObject(m.map({
+        case (k, v) => JField(
+          k match {
+            case ks: String => ks
+            case ks: Symbol => ks.name
+            case ks: Any => ks.toString
+          },
+          Extraction.decompose(v)
+        )
+      }).toList)
+    }
+
+    def deserialize(implicit format: Formats): PartialFunction[(TypeInfo, JValue), Map[Any, Any]] = {
+      sys.error("Not interested.")
+    }
   }
 }
